f2c976bc1f43fdabdcfeef266881575542ad7172
Atikur Rahman <mratikurrahman@gmail.com>
2023-10-29 23:30:10 +0600

    Added grid in AppleFrameworkView

--- AppleFrameworks/FrameworkHomeView.swift
+++ AppleFrameworks/FrameworkHomeView.swift
@@ -8,17 +8,39 @@
 import SwiftUI
 
 struct FrameworkHomeView: View {
+    let columns: [GridItem] = [GridItem(.flexible()),
+                             GridItem(.flexible()),
+                             GridItem(.flexible())]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        LazyVGrid(columns: columns) {
+            FrameworkCellView(name: "App Clips", imageName: "app-clip")
+            FrameworkCellView(name: "App Clips", imageName: "app-clip")
+            FrameworkCellView(name: "App Clips", imageName: "app-clip")
+            FrameworkCellView(name: "App Clips", imageName: "app-clip")
+            FrameworkCellView(name: "App Clips", imageName: "app-clip")
+            FrameworkCellView(name: "App Clips", imageName: "app-clip")
         }
-        .padding()
     }
 }
 
 #Preview {
     FrameworkHomeView()
+}
+
+struct FrameworkCellView: View {
+    let name: String
+    let imageName: String
+    
+    var body: some View {
+        VStack{
+            Image(imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+    }
 }
