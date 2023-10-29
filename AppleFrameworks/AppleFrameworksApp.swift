e4bd30a5e8ba495a2832ae57ea1f8c642409693b
Atikur Rahman <mratikurrahman@gmail.com>
2023-10-29 21:07:13 +0600

    The project has been created

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Atikur Rahman on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
