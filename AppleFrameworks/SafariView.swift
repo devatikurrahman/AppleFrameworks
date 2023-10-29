609cd6052d03357da0eab2dab6a55614e18c96fe
Atikur Rahman <mratikurrahman@gmail.com>
2023-10-30 03:05:14 +0600

    Created SafariView

---
+++ AppleFrameworks/SafariView.swift
@@ -0,0 +1,20 @@
+//
+//  SafariView.swift
+//  AppleFrameworks
+//
+//  Created by Atikur Rahman on 10/30/23.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {}
+}
